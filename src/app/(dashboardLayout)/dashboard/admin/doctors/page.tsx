6ac7183859eec2129dff462b3c21dda46f044d8f
arafat717 <arafatjibon33@gmail.com>
2025-07-23 18:22:23 +0600

    added doctor list and delete

--- src/app/(dashboardLayout)/dashboard/admin/doctors/page.tsx
+++ src/app/(dashboardLayout)/dashboard/admin/doctors/page.tsx
@@ -1,10 +1,79 @@
 "use client";
-import { Box, Button, Stack, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  IconButton,
+  Paper,
+  Stack,
+  TextField,
+} from "@mui/material";
 import { useState } from "react";
 import DoctorModal from "./components/DoctorModal";
+import {
+  useDeleteDoctorMutation,
+  useGetAllDoctorsQuery,
+} from "@/redux/api/doctorApi";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { GridDeleteIcon } from "@mui/x-data-grid";
+import { toast } from "sonner";
 
 const DoctorsPage = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const { data, isLoading } = useGetAllDoctorsQuery({});
+  console.log(data);
+  const doctors = data?.doctors;
+  const meta = data?.meta;
+
+  const [deleteDoctor] = useDeleteDoctorMutation();
+
+  const handleDelete = async (id: string) => {
+    try {
+      const res = await deleteDoctor(id).unwrap();
+      if (res.id) {
+        toast.success("Doctor deleted successfully!");
+      }
+    } catch (err: any) {
+      console.log(err);
+    }
+  };
+
+  const columns: GridColDef[] = [
+    {
+      field: "name",
+      headerAlign: "center",
+      align: "center",
+      headerName: "Title",
+      flex: 1,
+    },
+    {
+      field: "contactNumber",
+      headerAlign: "center",
+      align: "center",
+      headerName: "Contact Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerAlign: "center",
+      align: "center",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      flex: 1,
+      headerAlign: "center",
+      align: "center",
+      renderCell: ({ row }) => {
+        return (
+          <IconButton aria-label="delete" onClick={() => handleDelete(row.id)}>
+            <GridDeleteIcon />
+          </IconButton>
+        );
+      },
+    },
+  ];
 
   return (
     <Box>
@@ -13,6 +82,15 @@ const DoctorsPage = () => {
         <DoctorModal open={isModalOpen} setOpen={setIsModalOpen} />
         <TextField size="small" placeholder="search doctors" />
       </Stack>
+      {!isLoading ? (
+        <Box>
+          <Paper sx={{ width: "100%", mt: "20px" }}>
+            <DataGrid rows={doctors} columns={columns} sx={{ border: 0 }} />
+          </Paper>
+        </Box>
+      ) : (
+        <h1>Loading...</h1>
+      )}
     </Box>
   );
 };
